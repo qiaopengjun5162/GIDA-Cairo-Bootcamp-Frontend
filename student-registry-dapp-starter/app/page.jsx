1cc4287fb9e736f59283bbb6e5aa8d11cc2210f1
QiaoPengjun5162 <qiaopengjun0@gmail.com>
2024-10-21 14:14:34 +0800

    Add student finished

--- student-registry-dapp-starter/app/page.jsx
+++ student-registry-dapp-starter/app/page.jsx
@@ -1,31 +1,59 @@
 "use client";
+import { useContractRead, useAccount, useBalance } from "@starknet-react/core";
 import Balance from "./components/balance";
 import Header from "./components/header";
 import StudentsTable from "./components/students-table";
 import StudentsTableControl from "./components/students-table-control";
 import TotalStudents from "./components/total-students";
-import { dummyStudents } from "./lib/data";
+import { contractAddress, dummyStudents } from "./lib/data";
+import { ABI } from "./abis/abi";
+import Loading from "./components/loading";
 
 export default function Home() {
   // TODO - Fetch Students from Contract
+  const {
+    data: allStudents,
+    isLoading: isLoadingStudents,
+    refetch: refetchStudents,
+    isFetching: isFetchingStudents,
+  } = useContractRead({
+    functionName: "get_all_students",
+    args: [],
+    abi: ABI,
+    address: contractAddress,
+  });
+
+  console.log(allStudents);
+  const { address, status } = useAccount();
+  const { data, error } = useBalance({
+    address,
+    token: "0x04718f5a0fc34cc1af16a1cdee98ffb20c31f5cd61d6ab07201858f4287c938d",
+  });
 
   return (
     <div className="py-[60px] px-[100px]">
       <Header />
-      <div className="mt-[60px]">
-        <div className="flex justify-between items-center">
+      {isLoadingStudents || isFetchingStudents ? (
+        <Loading message="Fetching students..." />
+      ) : (
+        <div className="mt-[60px]">
+          <div className="flex justify-between items-center">
+            {/* TODO: - Pass correct students length */}
+            <TotalStudents total={allStudents?.length} />
+            <Balance />
+          </div>
+          {/* TODO: - Pass correct students */}
+          <StudentsTable students={allStudents ? allStudents : []} />
           {/* TODO: - Pass correct students length */}
-          <TotalStudents total={dummyStudents?.length} />
-          <Balance />
+          <StudentsTableControl
+            count={allStudents ? allStudents.length : 0}
+            handleRefreshStudents={() => {
+              refetchStudents();
+              console.log("Handle refetch");
+            }}
+          />
         </div>
-        {/* TODO: - Pass correct students */}
-        <StudentsTable students={dummyStudents} />
-        {/* TODO: - Pass correct students length */}
-        <StudentsTableControl
-          count={dummyStudents?.length}
-          handleRefreshStudents={() => console.log("Handle refetch")}
-        />
-      </div>
+      )}
     </div>
   );
 }
